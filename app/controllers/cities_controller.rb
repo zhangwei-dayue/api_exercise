549c58d31af7af3888094783c73b1c2a55ef8c3e
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-18 20:44:57 +0800

    增加更新温度功能

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,4 +2,12 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
+
+  def update_temp
+    city = City.find(params[:id])
+    response = RestClient.get "http://v.juhe.cn/weather/index", :params => { :cityname => city.juhe_id, :key => "72fefa4e644ead90f3d3f45f763a741f"}
+    data = JSON.parse(response.body)
+    city.update( :current_temp => data["result"]["sk"]["temp"])
+    redirect_to cities_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cities
+  resources :cities do
+    member do
+      post :update_temp
+    end
+  end
 end
