36916dcc6608425899c4af6279f5fe13f808b5d1
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-19 20:26:23 +0800

    显示用户所有订单:

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,4 +1,21 @@
 class Api::V1::ReservationsController < ApiController
+  before_action :authenticate_user!, :only => [:index]
+
+  def index
+    @reservations = current_user.reservations
+
+    render :json => {
+      :data => @reservations.map { |reservation|
+        {
+          :booking_code => reservation.booking_code,
+          :train_number => reservation.train.number,
+          :seat_number => reservation.seat_number,
+          :customer_name => reservation.customer_name,
+          :customer_phone => reservation.customer_phone
+        }
+      }
+    }
+  end
 
   def create
     @train = Train.find_by_number!(params[:train_number])
