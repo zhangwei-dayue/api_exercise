4c4f9ff4c828aafc85dd402514de8810ce8b9e83
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-19 20:16:25 +0800

    修改订票API

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::ReservationsController < ApiController
   def create
     @train = Train.find_by_number!(params[:train_number])
     @reservation = Reservation.new(:train_id => @train.id,:seat_number => params[:seat_number], :customer_name => params[:customer_name], :customer_phone => params[:customer_phone])
+    @reservation.user = current_user
     if @reservation.save
       render :json => {:booking_code => @reservation.booking_code, :reservation_url => api_v1_reservation_url(@reservation.booking_code)}
     else
